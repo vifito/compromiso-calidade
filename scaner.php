52f6b2fcca46f0f818dd0aa3094df6737f689641
vifito <vifito@gmail.com>
2015-12-22 13:53:19 +0100

    extraer imaxes dun PDF

--- scaner.php
+++ scaner.php
@@ -1,16 +1,26 @@
 <?php
 
+$pdf_in = 'images/SKMBT_C65215122213490.pdf';
+
 $img_array = array();
-$im = new imagick();
-$im->setResolution(150,150);
-$im->readImageBlob($pdf_in);
+$im = new Imagick();
+
+$im->setResolution(300, 300);
+
+$im->readImageBlob(file_get_contents($pdf_in));
 $num_pages = $im->getNumberImages();
-for($i = 0;$i < $num_pages; $i++) 
-{
+
+echo '$num_pages: ' . $num_pages;
+
+for($i=0; $i<$num_pages; $i++) {
     $im->setIteratorIndex($i);
     $im->setImageFormat('jpeg');
-    $img_array[$i] = $im->getImageBlob();
- }
- $im->destroy();
+    //$img_array[$i] = $im->getImageBlob();
+    //$img_array[$i] = clone($im);
+    
+    $im->writeImage('images/' . $i . '.jpg');
+}
+$im->destroy();
  
- 
+//$formulario = new Imagick();
+//$formulario->readImageBlob($imageBlob);
