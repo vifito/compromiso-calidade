89a82a221d6976f117f4e588b9fccda31ca2b348
vifito <vifito@vifito.eu>
2015-12-23 00:05:20 +0100

    .

--- scaner.php
+++ scaner.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 $pdf_in = 'images/SKMBT_C65215122213490.pdf';
 
 $img_array = array();
@@ -23,4 +24,49 @@ for($i=0; $i<$num_pages; $i++) {
 $im->destroy();
  
 //$formulario = new Imagick();
-//$formulario->readImageBlob($imageBlob);
+//$formulario->readImageBlob($imageBlob); */
+
+$regions = [
+    'Q3' => [
+        'width' => 255,
+        'height' => 1218,
+        'x' => 348,
+        'y' => 483,
+    ],
+    'Q4' => [
+        'width' => 255,
+        'height' => 1218,
+        'x' => 600,
+        'y' => 483,
+    ],
+];
+
+
+$imagePath = './images/0.jpg';
+$imagick = new \Imagick(realpath($imagePath));
+$imagick2 = clone $imagick;
+//cropImage ($width, $height, $x, $y)
+$width  = $regions['Q3']['width'];
+$height = $regions['Q3']['height'];
+$x = $regions['Q3']['x'];
+$y = $regions['Q3']['y'];
+
+$imagick2->cropImage($width, $height, $x, $y);
+
+$imagePath = './images/square.jpg';
+$square = new \Imagick(realpath($imagePath));
+
+$similarity = null;
+$bestMatch = null;
+$comparison = $imagick2->subImageMatch($square, $bestMatch, $similarity);
+
+/*$similarity->setImageFormat('png');
+$similarity->writeImage('images/similarity.png');*/
+
+var_dump($similarity);
+var_dump($bestMatch);
+
+
+
+$comparison->setImageFormat('png');
+$comparison->writeImage('images/comparison.png');
