9ebee7fac0749d1ad91aae81c006f63769f06452
vifito <vifito@gmail.com>
2016-02-10 10:49:02 +0100

    Estilos en formulario de upload

---
+++ enquisa/web/js/fos_js_routes.js
@@ -0,0 +1,1 @@
+fos.Router.setData({"base_url":"","routes":{"enquisa_panel":{"tokens":[["variable","\/","[^\/]++","qid"],["text","\/admin\/enquisa\/panel"]],"defaults":[],"requirements":{"_method":"GET"},"hosttokens":[]}},"prefix":"","host":"localhost","scheme":"http"});
